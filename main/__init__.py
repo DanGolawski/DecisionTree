dfecf6555fe05895555e49833a3c64c10c3245ee
Daniel Golawski <dangolawski@gmail.com>
2019-09-10 20:09:25 +0200

    beginning

--- main/__init__.py
+++ main/__init__.py
@@ -1,1 +1,13 @@
-print('Hello from Decision Tree')
+import numpy as np
+import pandas as pd
+eps = np.finfo(float).eps
+from numpy import log2
+
+
+dataset = {'Taste':['Salty','Spicy','Spicy','Spicy','Spicy','Sweet','Salty','Sweet','Spicy','Salty'],
+       'Temperature':['Hot','Hot','Hot','Cold','Hot','Cold','Cold','Hot','Cold','Hot'],
+       'Texture':['Soft','Soft','Hard','Hard','Hard','Soft','Soft','Soft','Soft','Hard'],
+       'Eat':['No','No','Yes','No','Yes','Yes','No','Yes','Yes','Yes']}
+
+df = pd.DataFrame(dataset, columns=['Taste','Temperature','Texture','Eat'])
+
