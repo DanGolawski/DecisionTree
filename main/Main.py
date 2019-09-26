2a495d394f8c26ca36fa40417ae94d54c1386b74
Daniel Golawski <dangolawski@gmail.com>
2019-09-26 16:01:30 +0200

    entropy calculator class was added

---
+++ main/Main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import main.Managers.EntropyCalculator
+
+eps = np.finfo(float).eps
+
+dataset = {'Taste': ['Salty', 'Spicy', 'Spicy', 'Spicy', 'Spicy', 'Sweet', 'Salty', 'Sweet', 'Spicy', 'Salty'],
+           'Temperature': ['Hot', 'Hot', 'Hot', 'Cold', 'Hot', 'Cold', 'Cold', 'Hot', 'Cold', 'Hot'],
+           'Texture': ['Soft', 'Soft', 'Hard', 'Hard', 'Hard', 'Soft', 'Soft', 'Soft', 'Soft', 'Hard'],
+           'Eat': ['No', 'No', 'Yes', 'No', 'Yes', 'Yes', 'No', 'Yes', 'Yes', 'Yes']}
+
+df = pd.DataFrame(dataset, columns=['Taste', 'Temperature', 'Texture', 'Eat'])
+
+calculator = main.Managers.EntropyCalculator.EntropyCalculator()
+
+# 1. Root Node Entropy
+rne = calculator.calculateRootNodeEntropy(df['Eat'].to_list())
+
+# 2. Gain for every attribute
+for attr in df.columns[:-1]:
+    # calculator.calculateGainForAttribute(rne, {attr: df[attr].to_list(), 'Eat': df['Eat'].to_list()})
+    calculator.calculateGainForAttribute(rne, df[[attr, 'Eat']])
---
+++ main/Managers/EntropyCalculator.py
@@ -0,0 +1,32 @@
+from math import log2
+
+
+class EntropyCalculator:
+
+    def calculateRootNodeEntropy(self, results):
+        values = set(results)
+        calculation = 0
+        for v in values:
+            proportion = results.count(v) / len(results)
+            calculation -= proportion * log2(proportion)
+        return calculation
+
+    def calculateGainForAttribute(self, entropy, list):
+        col_names = list.columns.to_list()
+        unique_attr = list.iloc[:, 0].unique()
+        unique_reslt = list.iloc[:, -1].unique()
+        # print(list[(list[col_names[0]]==unique_attr[0]) & (list[col_names[-1]]==unique_reslt[0])])
+        entropy = 0
+        # print(list[list.iloc[:,0]==unique[0]])
+        for ua in unique_attr:
+            proportion = len(list[list.iloc[:, 0] == ua]) / len(list)
+            # print(ua + " proportion..." + str(proportion))
+            internal_sum = 0
+            for ur in unique_reslt:
+                internal_proportion = len(list[(list[col_names[0]] == ua) & (list[col_names[-1]] == ur)]) / len(
+                    list[list[col_names[0]] == ua])
+                if internal_proportion != 0:
+                    internal_sum += internal_proportion * log2(internal_proportion)
+            entropy += -proportion * internal_sum
+            print(ua + '...' + str(entropy))
+        return entropy
