13f822d887410a6f5376c154e4b793a3f623cb06
Daniel Golawski <daniel.golawski@santander.pl>
2019-09-30 11:49:12 +0200

    before pull

--- main/Main.py
+++ main/Main.py
@@ -4,6 +4,9 @@ import main.Managers.EntropyCalculator
 
 eps = np.finfo(float).eps
 
+
+
+
 dataset = {'Taste': ['Salty', 'Spicy', 'Spicy', 'Spicy', 'Spicy', 'Sweet', 'Salty', 'Sweet', 'Spicy', 'Salty'],
            'Temperature': ['Hot', 'Hot', 'Hot', 'Cold', 'Hot', 'Cold', 'Cold', 'Hot', 'Cold', 'Hot'],
            'Texture': ['Soft', 'Soft', 'Hard', 'Hard', 'Hard', 'Soft', 'Soft', 'Soft', 'Soft', 'Hard'],
@@ -17,7 +20,10 @@ calculator = main.Managers.EntropyCalculator.EntropyCalculator()
 rne = calculator.calculateRootNodeEntropy(df['Eat'].to_list())
 
 # 2. Gain for every attribute
+attributes_with_gains = []
 for attr in df.columns[:-1]:
-    # calculator.calculateGainForAttribute(rne, {attr: df[attr].to_list(), 'Eat': df['Eat'].to_list()})
-    print(calculator.calculateGainForAttribute(rne, df[[attr, 'Eat']]))
+    calculator.calculateGainForAttribute(rne, {attr: df[attr].to_list(), 'Eat': df['Eat'].to_list()})
+    # print(calculator.calculateGainForAttribute(rne, df[[attr, 'Eat']]))
+
+
 
